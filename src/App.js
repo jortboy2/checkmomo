f6e38f90ecaef61d8bc1edc7189a3d2257950fe6
thanh phong <vonghung849@gmail.com>
2025-03-18 10:08:49 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,526 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { enqueueSnackbar } from "notistack";
+import React, { Fragment, useEffect, useState } from "react";
 
-function App() {
+function Sidebar({ setActiveMenu }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-1/4 bg-gradient-to-b from-green-500 to-green-700 text-white p-6 min-h-screen shadow-2xl rounded-r-3xl">
+      <h2 className="text-3xl font-extrabold mb-8 text-center">Menu</h2>
+      <ul className="space-y-4">
+        <li
+          className="py-3 px-4 cursor-pointer bg-green-600 hover:bg-green-800 rounded-xl text-lg text-center shadow-md"
+          onClick={() => setActiveMenu("dashboard")}
         >
-          Learn React
-        </a>
-      </header>
+          Quản lý
+        </li>
+        <li
+          className="py-3 px-4 cursor-pointer bg-green-600 hover:bg-green-800 rounded-xl text-lg text-center shadow-md"
+          onClick={() => setActiveMenu("paylater")}
+        >
+          Tự động khóa ví
+        </li>
+      </ul>
     </div>
   );
 }
 
-export default App;
+function Paylayter() {
+  const baseUrl = "http://localhost:4000";
+  const [data, setData] = useState([]);
+  const [phoneNumbers, setPhoneNumbers] = useState("");
+  const [phoneCount, setphoneCount] = useState([]);
+  const [savedPhones, setSavedPhones] = useState([]); // LƯU CÁC SỐ ĐÃ GỬI
+  const today = new Date();
+  const startOfMonth = new Date(today.getFullYear(), today.getMonth(), 1)
+    .toISOString()
+    .split("T")[0];
+  const endOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0)
+    .toISOString()
+    .split("T")[0];
+
+  const handleSubmit = async () => {
+    const phonesArray = phoneNumbers
+      .split("\n")
+      .map((phone) => phone.trim())
+      .filter((phone) => phone);
+
+    const uniquePhones = [...new Set([...savedPhones, ...phonesArray])];
+
+    setSavedPhones(uniquePhones); // CẬP NHẬT DANH SÁCH SỐ ĐÃ GỬI
+    setphoneCount(uniquePhones); // CẬP NHẬT HIỂN THỊ
+
+    for (let i = 0; i < uniquePhones.length; i++) {
+      try {
+        const response = await axios.post(`${baseUrl}/get-history`, {
+          phone: [uniquePhones[i]],
+          fromDate: startOfMonth,
+          toDate: endOfMonth,
+        });
+
+        console.log("Phản hồi từ server:", response.data.result_paylater);
+
+        setData((prevData) => {
+          const updatedData = [...prevData];
+          const existingIndex = updatedData.findIndex(
+            (item) => item.phone === uniquePhones[i]
+          );
+
+          if (existingIndex !== -1) {
+            // Cập nhật dữ liệu nếu đã tồn tại
+            updatedData[existingIndex] = {
+              ...updatedData[existingIndex],
+              ...response.data.result_paylater[0],
+            };
+          } else {
+            // Thêm mới nếu chưa có
+            updatedData.push(...response.data.result_paylater);
+          }
+
+          return updatedData;
+        });
+
+        enqueueSnackbar(
+          `Số ${uniquePhones[i]} xử lý thành công! - số tiền: ${response.data.result_paylater[0].totalMoney}`,
+          {
+            variant: "success",
+          }
+        );
+      } catch (error) {
+        console.error(`Lỗi khi xử lý số ${uniquePhones[i]}:`, error);
+        enqueueSnackbar(`Gửi thất bại cho số ${uniquePhones[i]}!`, {
+          variant: "error",
+        });
+      }
+
+      // Dừng 3 giây trước khi chạy số tiếp theo
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+    }
+
+    setPhoneNumbers(""); // XÓA Ô NHẬP SAU KHI GỬI
+  };
+
+  const handleDisablePaylayter = async (phone) => {
+    try {
+      const response = await axios.post(`${baseUrl}/paylater-disable`, {
+        phone: phone,
+      });
+      console.log("Phản hồi từ server:", response.data);
+      enqueueSnackbar("Khóa thành công! " + phone, {
+        variant: "success",
+      });
+    } catch (error) {
+      enqueueSnackbar(
+        `Gửi thất bại! ${
+          error.response.data.status || error.response.statusText
+        }`,
+        { variant: "error" }
+      );
+    }
+  };
+  useEffect(() => {
+    const handleSequentialDisable = async () => {
+      for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        console.log("Kiểm tra ví > 100tr");
+        if (item.totalMoney >= 100000000 && item.paylater) {
+          await handleDisablePaylayter(item.phone); // GỌI API KHÓA VÍ
+        }
+
+        // Chờ 5 giây trước khi xử lý số tiếp theo
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+      }
+    };
+
+    const interval = setInterval(() => {
+      handleSequentialDisable();
+    }, 10000); // 10 GIÂY
+
+    return () => clearInterval(interval); // DỌN DẸP INTERVAL KHI COMPONENT UNMOUNT
+  }, [data]);
+
+  return (
+    <div className="bg-white shadow-2xl rounded-3xl p-10 w-full border border-green-300">
+      <h1 className="text-4xl font-extrabold text-center text-green-600 mb-8">
+        Tự động khóa ví
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div>
+          <label className="block text-lg font-medium text-gray-700 mb-3">
+            Điện thoại
+          </label>
+          <textarea
+            className="w-full p-4 border border-green-400 rounded-xl focus:ring-4 focus:ring-green-500 focus:outline-none bg-green-50"
+            rows="6"
+            placeholder="Nhập số điện thoại, cách nhau bằng dấu phẩy..."
+            value={phoneNumbers}
+            onChange={(e) => setPhoneNumbers(e.target.value)}
+          ></textarea>
+        </div>
+        <div>
+          <label className="block text-lg font-medium text-gray-700 mb-3">
+            Mật khẩu
+          </label>
+          <input
+            type="text"
+            className="w-full p-4 border border-green-400 rounded-xl focus:ring-4 focus:ring-green-500 focus:outline-none bg-green-50"
+            placeholder="Nhập mật khẩu"
+          />
+        </div>
+      </div>
+      <span className="text-[20px]">
+        Đã thêm <span className="text-[red]">{phoneCount.length}</span> số điện
+        thoại
+      </span>
+
+      <button
+        className="mt-6 w-full bg-green-500 text-white py-3 rounded-2xl hover:bg-green-600 transition-all text-lg shadow-lg"
+        onClick={handleSubmit}
+      >
+        Xác nhận
+      </button>
+      <div className="mt-10 overflow-x-auto">
+        <table className="w-full border-collapse border border-green-400 shadow-lg rounded-xl">
+          <thead className="bg-green-500 text-white">
+            <tr>
+              <th className="py-3 px-4">STT</th>
+              <th className="py-3 px-4">Số điện thoại</th>
+              <th className="py-3 px-4">Tên Shop</th>
+              <th className="py-3 px-4">Tổng tiền</th>
+              <th className="py-3 px-4">Paylayter</th>
+              {/* <th className="py-3 px-4">% trả sau</th> */}
+              <th className="py-3 px-4">Tiền treo</th>
+              <th className="py-3 px-4">Trạng thái</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index} className="odd:bg-green-50 even:bg-green-100">
+                <td className="py-3 px-4 text-center">{index + 1}</td>
+                <td className="py-3 px-4">{item.phone}</td>
+                <td className="py-3 px-4">{item.shop}</td>
+                <td className="py-3 px-4 text-center">
+                  {item.totalMoney == null
+                    ? "0"
+                    : item.totalMoney.toLocaleString("vi-VN", {
+                        style: "currency",
+                        currency: "VND",
+                      })}
+                </td>
+                <td className="py-3 px-4">
+                  {item.paylater ? "Chưa khóa" : "Đã khóa"}
+                </td>
+                {/* <td className="py-3 px-4">{item.limit}</td> */}
+                <td className="py-3 px-4">{item.pendingMoney}</td>
+                <td className="py-3 px-4">{item.status}</td>
+
+                {/* <td className="py-3 px-4 text-center ">
+                  {item.paylater ? (
+                    <button
+                      onClick={() => handleDisablePaylayter(item.phone)}
+                      className="cursor-pointer bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition-all"
+                    >
+                      Khóa ví trả sau
+                    </button>
+                  ) : (
+                    "—" // Hoặc có thể để trống nếu muốn
+                  )}
+                </td> */}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+function Dashboard() {
+  const baseUrl = "http://localhost:4000";
+  const [data, setData] = useState([]);
+  const [phoneNumbers, setPhoneNumbers] = useState("");
+  const [phoneCount, setphoneCount] = useState([]);
+  const [savedPhones, setSavedPhones] = useState([]); // LƯU CÁC SỐ ĐÃ GỬI
+  const [fromDate, setFromDate] = useState(""); // Ngày bắt đầu
+  const [toDate, setToDate] = useState("");
+  const runSubmission = async () => {
+    await handleSubmit(); // Gọi hàm submit ban đầu
+    setTimeout(runSubmission, 5000); // Lặp lại sau 5 giây
+  };
+  const handleSubmit = async () => {
+    const phonesArray = phoneNumbers
+      .split("\n")
+      .map((phone) => phone.trim())
+      .filter((phone) => phone);
+
+    const uniquePhones = [...new Set([...savedPhones, ...phonesArray])];
+
+    setSavedPhones(uniquePhones); // CẬP NHẬT DANH SÁCH SỐ ĐÃ GỬI
+    setphoneCount(uniquePhones); // CẬP NHẬT HIỂN THỊ
+
+    for (let i = 0; i < uniquePhones.length; i++) {
+      try {
+        const response = await axios.post(`${baseUrl}/get-history`, {
+          phone: [uniquePhones[i]],
+          fromDate: fromDate,
+          toDate: toDate,
+        });
+
+        console.log("Phản hồi từ server:", response.data.result_paylater);
+
+        setData((prevData) => {
+          const updatedData = [...prevData];
+          const existingIndex = updatedData.findIndex(
+            (item) => item.phone === uniquePhones[i]
+          );
+
+          if (existingIndex !== -1) {
+            // Cập nhật dữ liệu nếu đã tồn tại
+            updatedData[existingIndex] = {
+              ...updatedData[existingIndex],
+              ...response.data.result_paylater[0],
+            };
+          } else {
+            // Thêm mới nếu chưa có
+            updatedData.push(...response.data.result_paylater);
+          }
+
+          return updatedData;
+        });
+
+        enqueueSnackbar(
+          `Số ${uniquePhones[i]} xử lý thành công! - số tiền: ${response.data.result_paylater[0].totalMoney}`,
+          {
+            variant: "success",
+          }
+        );
+      } catch (error) {
+        console.error(`Lỗi khi xử lý số ${uniquePhones[i]}:`, error);
+        enqueueSnackbar(`Gửi thất bại cho số ${uniquePhones[i]}!`, {
+          variant: "error",
+        });
+      }
+
+      // Dừng 3 giây trước khi chạy số tiếp theo
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    }
+
+    setPhoneNumbers(""); // XÓA Ô NHẬP SAU KHI GỬI
+  };
+
+  const handleDisablePaylayter = async (phone) => {
+    try {
+      const response = await axios.post(`${baseUrl}/paylater-disable`, {
+        phone: phone,
+      });
+      console.log("Phản hồi từ server:", response.data);
+      enqueueSnackbar("Lấy dữ liệu thành công! " + response.data.status, {
+        variant: "success",
+      });
+    } catch (error) {
+      enqueueSnackbar(
+        `Gửi thất bại! ${
+          error.response.data.status || error.response.statusText
+        }`,
+        { variant: "error" }
+      );
+    }
+  };
+  return (
+    <div className="bg-white shadow-2xl rounded-3xl p-10 w-full border border-green-300">
+      <h1 className="text-4xl font-extrabold text-center text-green-600 mb-8">
+        Kiểm tra ví
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div>
+          <div>
+            <label className="block text-lg font-medium text-gray-700 mb-3">
+              Ngày bắt đầu
+            </label>
+            <input
+              type="date"
+              className="w-full p-4 border border-green-400 rounded-xl focus:ring-4 focus:ring-green-500 focus:outline-none bg-green-50"
+              value={fromDate}
+              onChange={(e) => setFromDate(e.target.value)}
+            />
+          </div>
+          <div>
+            <label className="block text-lg font-medium text-gray-700 mb-3">
+              Ngày kết thúc
+            </label>
+            <input
+              type="date"
+              className="w-full p-4 border border-green-400 rounded-xl focus:ring-4 focus:ring-green-500 focus:outline-none bg-green-50"
+              value={toDate}
+              onChange={(e) => setToDate(e.target.value)}
+            />
+          </div>
+          <label className="block text-lg font-medium text-gray-700 mb-3">
+            Điện thoại
+          </label>
+          <textarea
+            className="w-full p-4 border border-green-400 rounded-xl focus:ring-4 focus:ring-green-500 focus:outline-none bg-green-50"
+            rows="6"
+            placeholder="Nhập số điện thoại, cách nhau bằng dấu phẩy..."
+            value={phoneNumbers}
+            onChange={(e) => setPhoneNumbers(e.target.value)}
+          ></textarea>
+        </div>
+        <div>
+          <label className="block text-lg font-medium text-gray-700 mb-3">
+            Mật khẩu
+          </label>
+          <input
+            type="text"
+            className="w-full p-4 border border-green-400 rounded-xl focus:ring-4 focus:ring-green-500 focus:outline-none bg-green-50"
+            placeholder="Nhập mật khẩu"
+          />
+        </div>
+      </div>
+      <span className="text-[20px]">
+        Đã thêm <span className="text-[red]">{phoneCount.length}</span> số điện
+        thoại
+      </span>
+
+      <button
+        className="mt-6 w-full bg-green-500 text-white py-3 rounded-2xl hover:bg-green-600 transition-all text-lg shadow-lg"
+        onClick={runSubmission}
+      >
+        Xác nhận
+      </button>
+      <div className="mt-10 overflow-x-auto">
+        <table className="w-full border-collapse border border-green-400 shadow-lg rounded-xl">
+          <thead className="bg-green-500 text-white">
+            <tr>
+              <th className="py-3 px-4">STT</th>
+              <th className="py-3 px-4">Số điện thoại</th>
+              <th className="py-3 px-4">Tên Shop</th>
+              <th className="py-3 px-4">Tổng tiền</th>
+              <th className="py-3 px-4">Paylayter</th>
+              <th className="py-3 px-4">% trả sau</th>
+              <th className="py-3 px-4">Tiền treo</th>
+              <th className="py-3 px-4">Trạng thái</th>
+              <th className="py-3 px-4">Hành động</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index} className="odd:bg-green-50 even:bg-green-100">
+                <td className="py-3 px-4 text-center">{index + 1}</td>
+                <td className="py-3 px-4">{item.phone}</td>
+                <td className="py-3 px-4">{item.shop}</td>
+                <td className="py-3 px-4 text-center">
+                  {item.totalMoney == null
+                    ? "0"
+                    : item.totalMoney.toLocaleString("vi-VN", {
+                        style: "currency",
+                        currency: "VND",
+                      })}
+                </td>
+                <td className="py-3 px-4">
+                  {item.paylater ? "Chưa khóa" : "Đã khóa"}
+                </td>
+                <td className="py-3 px-4">{Math.ceil(item.totalMoney / 1000000)} %</td>
+                <td className="py-3 px-4">{item.pendingMoney}</td>
+                <td className="py-3 px-4">{item.status}</td>
+
+                <td className="py-3 px-4 text-center ">
+                  {item.paylater ? (
+                    <button
+                      onClick={() => handleDisablePaylayter(item.phone)}
+                      className="cursor-pointer bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition-all"
+                    >
+                      Khóa ví trả sau
+                    </button>
+                  ) : (
+                    "—" // Hoặc có thể để trống nếu muốn
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  const baseUrl = "http://localhost:4000";
+  const [activeMenu, setActiveMenu] = useState("dashboard");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async () => {
+    try {
+      const response_login = await axios.post(`${baseUrl}/login`, {
+        email: email,
+        password: password,
+      });
+
+      if (response_login.status === 200) {
+        setIsLoggedIn(true);
+        enqueueSnackbar(response_login.data.message, {
+          variant: "success",
+        });
+      }
+    } catch (error) {
+      if (error.response) {
+        enqueueSnackbar(error.response.data.error, {
+          variant: "error",
+        });
+      } else {
+        enqueueSnackbar("Lỗi không xác định. Vui lòng thử lại sau.", {
+          variant: "error",
+        });
+      }
+    }
+  };
+
+  return (
+    <div>
+      {!isLoggedIn ? (
+        <div className="w-full h-screen flex justify-center items-center">
+          <div className="bg-white shadow-2xl rounded-3xl p-10 w-96 max-w-md mx-auto border border-green-300 ">
+            <h1 className="text-4xl font-extrabold text-center text-green-600 mb-8">
+              Đăng nhập
+            </h1>
+
+            <div className="space-y-6">
+              <input
+                type="text"
+                className="w-full p-4 border border-green-400 rounded-xl focus:ring-4 focus:ring-green-500 focus:outline-none bg-green-50"
+                placeholder="Nhập email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <input
+                type="password"
+                className="w-full p-4 border border-green-400 rounded-xl focus:ring-4 focus:ring-green-500 focus:outline-none bg-green-50"
+                placeholder="Nhập mật khẩu"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <button
+              className="mt-6 w-full bg-green-500 text-white py-3 rounded-2xl hover:bg-green-600 transition-all text-lg shadow-lg"
+              onClick={handleLogin}
+            >
+              Đăng nhập
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="flex min-h-screen bg-gradient-to-br from-green-100 to-green-200">
+          <Sidebar setActiveMenu={setActiveMenu} />
+          <div className="flex w-3/4 p-8">
+            {activeMenu === "dashboard" && <Dashboard />}
+            {activeMenu === "paylater" && <Paylayter />}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./output.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { SnackbarProvider } from "notistack";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
     <App />
-  </React.StrictMode>
+  </SnackbarProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
